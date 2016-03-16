ee76d2de01bc51a0929d4db4a6e6dda3b35c1569
Harish Pillai <paharish@yahoo.com>
2016-03-16 22:56:13 +0000

    First commit

---
+++ src/test/java/group1/artifact1/FirstTest.java
@@ -0,0 +1,64 @@
+package group1.artifact1;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@DataProvider( name = "dataFactory")
+	public Object [][] dataGen(){
+		Object [][] data = { { "arg00", "arg01" }, { "arg10", "arg11" } };
+		return data;
+	}
+	
+	@BeforeSuite
+	public void beforeSuite(){
+		System.out.println("From Before Suite ...");
+	}
+	
+	@BeforeTest
+	public void beforeTest(){
+		System.out.println("From Before Test....");
+	}
+	
+	@BeforeClass
+	public void beforeClass(){
+		System.out.println("From Before Class.. <FirstTest>");
+	}
+	@Test(dataProvider = "dataFactory" )
+	public void test1( String arg1, String arg2 ){
+		System.out.println("From test1.... Arg1 : " + arg1 
+				+ " Arg2 is : " + arg2 );
+	}
+	
+	@Test
+	@Parameters( { "name", "age" } )
+	public void test2( String arg1, String arg2 ){
+		System.out.println("From test2");
+		System.out.println("Arg1 is : " + arg1 + " Arg2 is : " + arg2 );
+	}
+	
+	@AfterClass
+	public void afterClass(){
+		System.out.println("From After Class... <FirstTest>");
+	}
+	
+	@AfterTest
+	public void afterTest(){
+		System.out.println("From After Test....");
+	}
+	
+	@AfterSuite
+	public void afterSuite(){
+		System.out.println("From After Suite ....");
+	}
+	
+
+}
---
+++ src/test/java/group1/artifact1/SecondTest.java
@@ -0,0 +1,12 @@
+package group1.artifact1;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	
+	@Test
+	public void test3(){
+		System.out.println("From test3....");
+	}
+
+}
